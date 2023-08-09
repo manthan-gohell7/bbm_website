b28f6369ee8a4042290617ba58840611542d15a6
Manthan Gohel <manthan.gohel2671@gmail.com>
2023-08-10 01:53:16 +0530

    First Commit - Intro Screen
    Implemented the UI of intro screen

---
+++ src/routes/+page.svelte
@@ -0,0 +1,45 @@
+<svelte:head>
+	<title>Bhakti Bhajan Mantra Intro</title>
+	<meta name="Introduction" content="Bhakti Bhajan Mantra" />
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+	<title>Bhakti Bhajan Mantra</title>
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+		integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+		crossorigin="anonymous"
+	/>
+	<link rel="stylesheet" href="src/routes/styles.css" />
+</svelte:head>
+
+<body>
+	<div class="intro-screen">
+		<div class="mainContainer">
+			<div class="subContainer">
+				<div class="subContainer-group">
+					<img class="rightFlower" alt="Rs layer wrap rs" src="src/routes/img/rightFlower.png" />
+					<img class="img" alt="Rs layer wrap rs" src="src/routes/img/leftFlower.png" />
+					<img class="musicDisc spin" alt="Rs layer wrap rs" src="src/routes/img/musicDisc.png" />
+					<img class="trackImage" alt="Rs layer wrap link" src="src/routes/img/trackImage.png" />
+					<div class="amazonMusicContainer">
+						<img
+							class="amazonMusic"
+							alt="Rs layer wrap link"
+							src="src/routes/img/amazonMusic.png"
+						/>
+					</div>
+					<img class="playStore" alt="Rs layer wrap link" src="src/routes/img/playStore.png" />
+					<div><a class="enterSiteBtn" href="/home">ENTER SITE</a></div>
+				</div>
+
+				<header class="header">
+					<div class="headerPosition">
+						<h1 class="title">BHAKTI BHAJAN MANTRA</h1>
+					</div>
+				</header>
+			</div>
+		</div>
+	</div>
+</body>
---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+	<title>Bhakti Bhajan Mantra Home</title>
+	<meta name="Bhakti Bhajan Mantra Home" content="Bhakti Bhajan Mantra Home" />
+</svelte:head>
+
+<div>
+	<p>In Home</p>
+</div>
